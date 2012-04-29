8315aee6dbd022645abdf7a7b1150d193df2c87c
TiredJesse@gmail.com <TiredJesse@gmail.com@5c60be36-396f-8bfa-1965-f9bf8aee3763>
2012-04-29 02:49:31 +0000

    Expanded on the static section and the enumerator section.
    Changed some comments around to be more informative and more concise.

---  tiredjesse-scjp-study --username TiredJesse@gmail.com/Ch1-DeclarationsAndAccessControl/src/three/syntax/Enumerators.java
+++  tiredjesse-scjp-study --username TiredJesse@gmail.com/Ch1-DeclarationsAndAccessControl/src/three/syntax/Enumerators.java
@@ -1,33 +1,50 @@
 package three.syntax;
 
-enum OuterEnum{
+// Enums can only implement Interfaces
+// Since an enum is NOT a class, cannot extend a class nor be extended upon!
+enum OuterEnum implements Messenger {
 	ONE("constant one");
 	
 	private String msg;
 	
 	private OuterEnum(String msg) {
-		this.msg = msg;
+		setMsg(msg);
 	}
 	
+	@Override
 	public String getMsg() {
 		return msg;
+	}
+	
+	@Override
+	public void setMsg(String msg) {
+		this.msg = msg;
 	}
 } 
 
 public class Enumerators {
-	enum InnerEnum{
+	enum InnerEnum implements Messenger{
 		TWO("constant two") {
+			@Override
 			public String getMsg() {
 				return "overridden msg";
 			}
-		};
+
+		},
+		THREE("constant three");
 		
 		private String msg;
 		
 		private InnerEnum(String msg) {
+			setMsg(msg);
+		}
+
+		@Override
+		public void setMsg(String msg) {
 			this.msg = msg;
 		}
 		
+		@Override
 		public String getMsg() {
 			return msg;
 		}
@@ -36,5 +53,11 @@ public class Enumerators {
 	public static void main(String[] args) {
 		System.out.println(OuterEnum.ONE.getMsg()); // constant one
 		System.out.println(InnerEnum.TWO.getMsg()); // overridden msg
+		System.out.println(InnerEnum.THREE.getMsg()); // constant three
 	}
+}
+
+interface Messenger {
+	public abstract String getMsg();
+	void setMsg(String msg); // Note still public and abstract just not mentioned
 }
